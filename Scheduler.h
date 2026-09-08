04a11a16d88e005f0dcc2dc77e26196cf131cffa
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:20:14 +0530

    Update Scheduler.h

--- Scheduler.h
+++ Scheduler.h
@@ -1,1 +1,19 @@
 
+#pragma once
+
+#include <vector>
+#include "ProcessModel.h"
+#include "ExecutionResult.h"
+#include "AlgorithmType.h"
+
+class Scheduler
+{
+public:
+    virtual ~Scheduler();
+
+    virtual ExecutionResult schedule(
+        const std::vector<ProcessModel>& processes
+    ) = 0;
+
+    virtual AlgorithmType getAlgorithmType() const = 0;
+};

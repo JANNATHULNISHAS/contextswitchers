ddc7f8b27510923c23f34233ded10fa03f8d10f9
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:20:48 +0530

    Update SimulationEvent.h

--- SimulationEvent.h
+++ SimulationEvent.h
@@ -1,1 +1,28 @@
 
+#pragma once
+ 
+#include <string>
+ 
+#include "StateTransition.h"
+ 
+class SimulationEvent
+{
+private:
+    std::string processName;
+    int timestamp;
+    StateTransition transition;
+ 
+public:
+    SimulationEvent(
+        const std::string& processName,
+        int timestamp,
+        ProcessState from,
+        ProcessState to
+    );
+ 
+    const std::string& getProcessName() const;
+ 
+    int getTimestamp() const;
+ 
+    const StateTransition& getTransition() const;
+};

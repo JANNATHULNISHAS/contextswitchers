27a252e372c5824e2ff5a17fd467fb15ce4a6f81
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:14:49 +0530

    Update ExecutionSlice.h

--- ExecutionSlice.h
+++ ExecutionSlice.h
@@ -1,1 +1,24 @@
 
+#pragma once
+
+#include <string>
+
+class ExecutionSlice
+{
+private:
+    std::string processName;
+    int startTime;
+    int endTime;
+
+public:
+    ExecutionSlice(
+        const std::string& processName,
+        int startTime,
+        int endTime
+    );
+
+    const std::string& getProcessName() const;
+    int getStartTime() const;
+    int getEndTime() const;
+    int getDuration() const;
+};

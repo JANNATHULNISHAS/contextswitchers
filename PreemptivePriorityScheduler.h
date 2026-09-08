67bec1bd5dc4646854c8ac0a7b5a65d0c71736fc
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:16:08 +0530

    Update PreemptivePriorityScheduler.h

--- PreemptivePriorityScheduler.h
+++ PreemptivePriorityScheduler.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include "Scheduler.h"
+
+class PreemptivePriorityScheduler : public Scheduler
+{
+private:
+    static const int AGING_THRESHOLD = 5;
+
+public:
+    ExecutionResult schedule(
+        const std::vector<ProcessModel>& processes
+    ) override;
+
+    AlgorithmType getAlgorithmType() const override;
+};

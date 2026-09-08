8ee20b01905adbbbb0551ab2c5a8b2639978e55d
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:21:24 +0530

    Update StateTransition.h

--- StateTransition.h
+++ StateTransition.h
@@ -1,1 +1,15 @@
 
+#pragma once
+
+#include "ProcessState.h"
+
+struct StateTransition
+{
+    ProcessState from;
+    ProcessState to;
+
+    StateTransition(ProcessState fromState, ProcessState toState)
+        : from(fromState), to(toState)
+    {
+    }
+};

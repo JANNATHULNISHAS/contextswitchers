868ac6376f0be0c7603577d25bc3f6680e271856
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:16:24 +0530

    Update PriorityScheduler.h

--- PriorityScheduler.h
+++ PriorityScheduler.h
@@ -1,1 +1,14 @@
 
+#pragma once
+
+#include "Scheduler.h"
+
+class PriorityScheduler : public Scheduler
+{
+public:
+    ExecutionResult schedule(
+        const std::vector<ProcessModel>& processes
+    ) override;
+
+    AlgorithmType getAlgorithmType() const override;
+};

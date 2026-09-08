4b04548f4ae8b882893647a02312ee12a5fe53d0
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:15:41 +0530

    Update MetricsCalculator.h

--- MetricsCalculator.h
+++ MetricsCalculator.h
@@ -1,1 +1,16 @@
 
+#pragma once
+ 
+#include <vector>
+ 
+#include "ProcessModel.h"
+#include "ExecutionResult.h"
+ 
+class MetricsCalculator
+{
+public:
+    void calculate(
+        const std::vector<ProcessModel>& processes,
+        ExecutionResult& result
+    ) const;
+};

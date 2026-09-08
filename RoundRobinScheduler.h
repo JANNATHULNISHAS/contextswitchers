4b26ee7978aaa24249c9b684112d22896d896b14
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:18:28 +0530

    Update RoundRobinScheduler.h

--- RoundRobinScheduler.h
+++ RoundRobinScheduler.h
@@ -1,1 +1,21 @@
 
+#pragma once
+ 
+#include "Scheduler.h"
+ 
+class RoundRobinScheduler : public Scheduler
+{
+private:
+    int timeQuantum;
+ 
+public:
+    explicit RoundRobinScheduler(int quantum = 4);
+ 
+    ExecutionResult schedule(
+        const std::vector<ProcessModel>& processes
+    ) override;
+ 
+    AlgorithmType getAlgorithmType() const override;
+ 
+    int getTimeQuantum() const;
+};

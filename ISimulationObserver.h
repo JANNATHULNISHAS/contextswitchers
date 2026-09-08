8f314a154a923b6de5b425c18181fb77fc53a99a
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:15:25 +0530

    Update ISimulationObserver.h

--- ISimulationObserver.h
+++ ISimulationObserver.h
@@ -1,1 +1,16 @@
 
+#pragma once
+ 
+class SimulationEvent;
+ 
+class ISimulationObserver
+{
+public:
+    virtual ~ISimulationObserver()
+    {
+    }
+ 
+    virtual void onEvent(
+        const SimulationEvent& event
+    ) = 0;
+};

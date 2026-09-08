b6933aa88e6b5628b2f904f9756f410533ade84f
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:19:34 +0530

    Update SRTFScheduler.h

--- SRTFScheduler.h
+++ SRTFScheduler.h
@@ -1,1 +1,14 @@
 
+#pragma once
+
+#include "Scheduler.h"
+
+class SRTFScheduler : public Scheduler
+{
+public:
+    ExecutionResult schedule(
+        const std::vector<ProcessModel>& processes
+    ) override;
+
+    AlgorithmType getAlgorithmType() const override;
+};

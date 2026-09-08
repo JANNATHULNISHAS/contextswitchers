94d621631fe47e0ce481bc8a957f8d1c4e1ce16e
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:17:56 +0530

    Update RecommendationEngine.h

--- RecommendationEngine.h
+++ RecommendationEngine.h
@@ -1,1 +1,34 @@
 
+#pragma once
+ 
+#include "RecommendationResult.h"
+#include "WorkloadMetrics.h"
+ 
+class RecommendationEngine
+{
+public:
+    RecommendationResult recommend(
+        const WorkloadMetrics& metrics
+    ) const;
+ 
+private:
+    RecommendationResult recommendFCFS(
+        const WorkloadMetrics& metrics
+    ) const;
+ 
+    RecommendationResult recommendSJF(
+        const WorkloadMetrics& metrics
+    ) const;
+ 
+    RecommendationResult recommendSRTF(
+        const WorkloadMetrics& metrics
+    ) const;
+ 
+    RecommendationResult recommendPriority(
+        const WorkloadMetrics& metrics
+    ) const;
+ 
+    RecommendationResult recommendRoundRobin(
+        const WorkloadMetrics& metrics
+    ) const;
+};

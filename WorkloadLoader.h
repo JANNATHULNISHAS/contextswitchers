6d0a004bb2c9aa08106384ceb427161f48184565
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:22:42 +0530

    Update WorkloadLoader.h

--- WorkloadLoader.h
+++ WorkloadLoader.h
@@ -1,1 +1,14 @@
 
+#pragma once
+ 
+#include <string>
+#include <vector>
+#include "ProcessModel.h"
+ 
+class WorkloadLoader
+{
+public:
+    std::vector<ProcessModel> loadCSV(
+        const std::string& filePath
+    ) const;
+};

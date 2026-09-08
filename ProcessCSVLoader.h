606f423c50a3b23f54d03f06d269c1d6d1052b55
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:16:41 +0530

    Update ProcessCSVLoader.h

--- ProcessCSVLoader.h
+++ ProcessCSVLoader.h
@@ -1,1 +1,15 @@
 
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "ProcessModel.h"
+
+class ProcessCSVLoader
+{
+public:
+
+    std::vector<ProcessModel> load(
+        const std::string& filePath)const;
+};

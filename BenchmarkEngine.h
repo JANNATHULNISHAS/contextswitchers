75a130f84e70a8253f025252033ca68e4860c5ea
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:13:54 +0530

    Update BenchmarkEngine.h

--- BenchmarkEngine.h
+++ BenchmarkEngine.h
@@ -1,1 +1,21 @@
 
+#pragma once
+ 
+#include <vector>
+ 
+#include "ProcessModel.h"
+#include "BenchmarkResult.h"
+ 
+class BenchmarkEngine
+{
+public:
+    std::vector<BenchmarkResult> benchmark(
+        const std::vector<ProcessModel>& processes
+    ) const;
+ 
+private:
+    BenchmarkResult benchmarkAlgorithm(
+        const std::vector<ProcessModel>& processes,
+        AlgorithmType algorithm
+    ) const;
+};

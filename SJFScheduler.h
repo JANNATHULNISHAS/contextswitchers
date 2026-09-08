3b410a540b25931fd2d1387b3d1b7608934a5d04
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:19:18 +0530

    Update SJFScheduler.h

--- SJFScheduler.h
+++ SJFScheduler.h
@@ -1,1 +1,14 @@
 
+#pragma once
+
+#include "Scheduler.h"
+
+class SJFScheduler : public Scheduler
+{
+public:
+    ExecutionResult schedule(
+        const std::vector<ProcessModel>& processes
+    ) override;
+
+    AlgorithmType getAlgorithmType() const override;
+};

1a4d1f1368aea2a0992f46dceadf8b4a809fc391
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:17:28 +0530

    Update RankingEngine.h

--- RankingEngine.h
+++ RankingEngine.h
@@ -1,1 +1,20 @@
 
+#pragma once
+ 
+#include <vector>
+ 
+#include "BenchmarkResult.h"
+ 
+class RankingEngine
+{
+public:
+    std::vector<BenchmarkResult> rank(
+        const std::vector<BenchmarkResult>& results
+    ) const;
+ 
+private:
+    double calculateScore(
+        const BenchmarkResult& result
+    ) const;
+};
+ 

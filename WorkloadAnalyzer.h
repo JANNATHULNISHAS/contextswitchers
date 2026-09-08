ec902944a109bb56422d056c9819ea3323bcb420
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-08 10:22:23 +0530

    Update WorkloadAnalyzer.h

--- WorkloadAnalyzer.h
+++ WorkloadAnalyzer.h
@@ -1,1 +1,14 @@
 
+#pragma once
+
+#include <vector>
+#include "ProcessModel.h"
+#include "WorkloadMetrics.h"
+
+class WorkloadAnalyzer
+{
+public:
+    WorkloadMetrics analyze(
+        const std::vector<ProcessModel>& processes
+    ) const;
+};

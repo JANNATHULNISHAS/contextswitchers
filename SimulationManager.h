4201086660a1168a35a7a4e5629dacbebfca36b8
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-15 16:46:09 +0530

    Update SimulationManager.h

--- SimulationManager.h
+++ SimulationManager.h
@@ -1,4 +1,3 @@
-
 #pragma once
  
 #include <string>
@@ -9,28 +8,70 @@
 #include "RecommendationResult.h"
 #include "BenchmarkResult.h"
 #include "ExecutionResult.h"
- 
+#include "SimulationEvent.h"
+
 class SimulationManager
 {
 public:
-    void run(
+  /*  void run(
         const std::string& csvFilePath
-    );
- 
+    );*/
+    void run(
+    const std::string& csvFilePath,
+    const std::string& applicationName
+);
 private:
     void displayWorkloadMetrics(
         const WorkloadMetrics& metrics
     ) const;
- 
+
+    void generateWorkloadAnalysisReport(
+    const std::string& csvFilePath,
+    const std::string& applicationName,
+    const WorkloadMetrics& metrics,
+    const RecommendationResult& recommendation
+    ) const;
+
+    void generateSummaryReport(
+    const std::string& applicationName,
+    const RecommendationResult& recommendation,
+    const ExecutionResult& executionResult,
+    const std::vector<BenchmarkResult>& rankedResults
+    ) const;
+
+    void generateBenchmarkReport(
+    const std::string& applicationName,
+    const std::vector<BenchmarkResult>& rankedResults,
+    const RecommendationResult& recommendation
+    ) const;
+    
+    void generateProcessReport(
+    const std::vector<ProcessModel>& processes,
+    const ExecutionResult& executionResult
+    ) const;
+
+    void generateStateTransitionReport(
+    const std::string& applicationName,
+    const std::vector<SimulationEvent>& transitions
+) const;
+
     void displayRecommendation(
         const RecommendationResult& recommendation
     ) const;
-    void runRecommendedAlgorithm(
-        const std::vector<ProcessModel>& processes,
-        const RecommendationResult& recommendation
+
+    ExecutionResult runRecommendedAlgorithm(
+    const std::vector<ProcessModel>& processes,
+    const RecommendationResult& recommendation,
+    const std::string& applicationName
     ) const;
+
+    void displayRecommendedExecution(
+    const ExecutionResult& result
+) const;
  
- 
+//bool askRunOtherAlgorithms() const;
+    
+
     void displayBenchmarkResults(
         const std::vector<BenchmarkResult>& results
     ) const;
@@ -46,6 +87,10 @@ private:
 
     void displayExecutionTimeline(
         const ExecutionResult& result
+    ) const;
+    
+    void displayGanttChart(
+    const ExecutionResult& result
     ) const;
 
     void displaySRTFVerification(

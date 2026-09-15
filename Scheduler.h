6046dd8d80eddd3dc0a884a3ef2383e7553550e8
JANNATHULNISHAS <jannathul.nisha@hcltech.com>
2026-09-15 16:44:02 +0530

    Update Scheduler.h

--- Scheduler.h
+++ Scheduler.h
@@ -1,19 +1,37 @@
-
 #pragma once
-
+ 
 #include <vector>
+ 
 #include "ProcessModel.h"
 #include "ExecutionResult.h"
 #include "AlgorithmType.h"
-
+#include "ISimulationObserver.h"
+ 
 class Scheduler
 {
+private:
+    std::vector<ISimulationObserver*> observers;
+ 
 public:
+    Scheduler();
     virtual ~Scheduler();
-
+ 
+    void setObserver(ISimulationObserver* observer);
+    void addObserver(ISimulationObserver* observer);
+ 
+protected:
+    void notifyEvent(const SimulationEvent& event) const;
+ 
+    void changeProcessState(
+        ProcessModel& process,
+        ProcessState newState,
+        int timestamp
+    ) const;
+ 
+public:
     virtual ExecutionResult schedule(
         const std::vector<ProcessModel>& processes
     ) = 0;
-
+ 
     virtual AlgorithmType getAlgorithmType() const = 0;
 };
